8b782f81ea285b9c122805bf9e2be872085fc501
Ardovski <uardaozdes@gmail.com>
2025-06-23 16:22:23 +0300

    last

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: uozdes <uardaozdes@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 20:04:06 by uozdes            #+#    #+#             */
-/*   Updated: 2025/06/23 16:04:34 by uozdes           ###   ########.fr       */
+/*   Updated: 2025/06/23 16:19:58 by uozdes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,5 @@ int	ft_putnbr(int n);
 int	ft_putnbr_u(unsigned int n);
 int	ft_puthex(unsigned long n, int uppercase);
 int	ft_putptr(void *ptr);
-
-typedef struct s_format
-{
-	int flag_minus;
-	int flag_zero;
-	int flag_plus;
-	int flag_space;
-	int flag_hash;
-	int width;
-	int precision;
-	int precision_specified;
-	char conversion;
-} t_format;
 
 #endif
